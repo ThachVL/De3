33553a43c013bebe5319039a443f668621beeba9
Stonsst <vulamthach10022000@gmai.com>
2022-07-15 16:28:33 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<script>
+    
+</script>
+<body>
+    <div class="wrapper">
+        <!-- header -->
+        <div class="header"></div>
+        <!-- end header -->
+        <!-- menu -->
+        <div class="navbar" id="menu"></div>
+        <!-- end menu -->
+        <!-- main content -->
+        <div class="main-content"></div>
+            <!-- Slider -->
+            <div class="slider"></div>
+            <!-- end slider -->
+            <!-- Breadcrumb -->
+            <div class="breadcrumb"></div>
+            <!-- Breadcrumb -->
+            <div class="colums">
+                <!-- Left Sider -->
+                <aside class="left-sidebar"></aside>
+                <!-- end left sider -->
+                <!-- page-main -->
+                <div class="page-main">
+                    <!-- banner -->
+                    <div class="banner"></div>
+                    <!-- end banner -->
+                    <!-- top-selling -->
+                    <div class="top-selling"></div>
+                    <!-- end top-selling -->
+                    <!-- news -->
+                    <div class="news"></div>
+                    <!-- end news -->
+                    <!-- newsletter -->
+                    <div class="newsletter"></div>
+                    <!-- End newsletter -->
+                </div>
+                <!-- end page-main -->
+                <aside class="right-sidebar"></aside>
+            </div>
+            
+        </div>
+        <!-- End main content -->
+    </div>
+</body>
+</html>
+<?php 
+    echo "124354566";
+?>
